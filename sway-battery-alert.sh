0cbd82d468e7099a59a528b52bed4da98bd49f6b
Martin Mihalkov <martinmihalkov4@gmail.com>
2026-06-26 18:53:30 +0300

    EDIT sway-battery-alert: remove the top threshold
    Due to strange random flickering and triggering
    the high threshold bar status randomly.

--- sway-battery-alert.sh
+++ sway-battery-alert.sh
@@ -1,16 +1,12 @@
 #!/bin/sh
 
-# CONFIGURATION
-BATTERY="BAT0"                     # Change to BAT1 if your system uses it
-BOTTOM_THRESHOLD=20                   # Threshold percentage to turn the bar red
-TOP_THRESHOLD=80                   # Threshold percentage to turn the bar red
-
-CHECK_INTERVAL=1                  # How often to check in seconds
+# CONFIGURATION BATTERY="BAT0"                     # Change to BAT1 if your system uses it
+WARNING_LEVEL=20                   # Threshold percentage to turn the bar red
+CHECK_INTERVAL=10                  # How often to check in seconds
 
 # BAR COLORS (Hex format)
 COLOR_NORMAL="#1d2021"             # Your default status bar background color
-RED_ALERT="#cc241d"              # Bright red for low battery alert
-GREEN_ALERT="#00bb00"              # Bright red for low battery alert
+COLOR_ALERT="#cc241d"              # Bright red for low battery alert
 
 # Track the last applied state to avoid spamming swaymsg
 LAST_STATE="NORMAL"
@@ -21,29 +17,18 @@ while true; do
         STATUS=$(cat "/sys/class/power_supply/$BATTERY/status")
 
         # Trigger alert if discharging AND below or equal to threshold
-        if [ "$STATUS" = "Discharging" ] && [ "$CAPACITY" -le "$BOTTOM_THRESHOLD" ]; then
+        if [ "$STATUS" = "Discharging" ] && [ "$CAPACITY" -le "$WARNING_LEVEL" ]; then
             if [ "$LAST_STATE" != "ALERT" ]; then
                 # Update all connected bars to the alert background color
-                echo "HIHI"
-                swaymsg "bar bar-0 colors background '$RED_ALERT'"
+                swaymsg "bar bar-0 colors background $COLOR_ALERT"
                 LAST_STATE="ALERT"
             fi
-            break
-        elif [ "$STATUS" = "Charging" ] && [ "$CAPACITY" -gt "$TOP_THRESHOLD" ]; then
-            if [ "$LAST_STATE" != "ALERT" ]; then
-                # Update all connected bars to the alert background color
-                echo "HIHI"
-                swaymsg "bar bar-0 colors background '$GREEN_ALERT'"
-                LAST_STATE="ALERT"
-            fi
-            break
         else
             if [ "$LAST_STATE" != "NORMAL" ]; then
                 # Restore the clean default background color
-                swaymsg "bar bar-0 colors background '$COLOR_NORMAL'"
+                swaymsg "bar bar-0 colors background $COLOR_NORMAL"
                 LAST_STATE="NORMAL"
             fi
-            break
         fi
     fi
     sleep "$CHECK_INTERVAL"
